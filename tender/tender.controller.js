eca70a56855517c159332fb045e27268c666df02
mefy-neha <neha.keshri@mefy.care>
2019-06-12 17:35:02 +0530

    small change

--- tender/tender.controller.js
+++ tender/tender.controller.js
@@ -21,7 +21,7 @@ router.post('/tender', (request, response) => {
 
     let y = []
     let sentResponse = {};
-    let newCompanyData = []
+    let newCompanyData = {};
     company.findOne({ superAdminId: superAdminId }, (error, result) => {
         // console.log('error', error);
         // console.log('result', result);
@@ -31,73 +31,17 @@ router.post('/tender', (request, response) => {
             response.status(500).json(sentResponse);
         }
         else if (result) {
-            // console.log("result>>>>>>>>>>>>" + result)       
-            if (companyData.gst == 'gst') {
-                console.log("gst")
-                newCompanyData.push({ gst: result.gst })
-            }
-            if (companyData.road_registration_certificate == 'road_registration_certificate') {
-                console.log("road_registration_certificate")
-                newCompanyData.push({ road_registration_certificate: result.road_registration_certificate })
-            }
-            if (companyData.tradeLicense_A == 'tradeLicense_A') {
-                console.log("tradeLicense_A")
-                newCompanyData.push({ tradeLicense_A: result.tradeLicense_A })
+            for (var key in companyData){
+                newCompanyData[key] = result[key];
+                delete newCompanyData.companyId;
+                newCompanyData.companyId=result._id;
+                newCompanyData.userId=result.userId;
+                newCompanyData.organisation=result.organisation;
 
+                // newFleetData.ownership=result.ownership;
             }
-            if (companyData.tradeLicense_B == 'tradeLicense_B') {
-                console.log("tradeLicense_B")
-                newCompanyData.push({ tradeLicense_B: result.tradeLicense_B })
-
-            }
-            if (companyData.invoice_number == 'invoice_number') {
-                console.log("invoice_number")
-                newCompanyData.push({ invoice_number: result.invoice_number })
-
-            }
-            if (companyData.pan == 'pan') {
-                console.log("pan")
-                newCompanyData.push({ pan: result.pan })
-
-            }
-            if (companyData.tan == 'tan') {
-                console.log("tan")
-                newCompanyData.push({ tan: result.tan })
-
-            }
-            if (companyData.pf == 'pf') {
-                console.log("pf")
-                newCompanyData.push({ pf: result.pf })
-
-            }
-            if (companyData.professional_tax == 'professional_tax') {
-                console.log("professional_tax")
-                newCompanyData.push({ professional_tax: result.professional_tax })
-
-            }
-            if (companyData.esi == 'esi') {
-                console.log("esi")
-                newCompanyData.push({ esi: result.esi })
-
-            }
-            if (companyData.itr == 'itr') {
-                console.log("itr")
-                newCompanyData.push({ itr: result.itr })
-
-            }
-            if (companyData.balance_sheet == 'balance_sheet') {
-                console.log("balance_sheet")
-                newCompanyData.push({ balance_sheet: result.balance_sheet })
-
-            }
-            if (companyData.others == 'others') {
-                console.log("others")
-                newCompanyData.push({ balance_sheet: result.others })
-
-            }
-
             if (fleetArray != null && fleetArray.length != 0) {
-                console.log("fleet>>>>>>>>>>>>>>>>>>>>")
+                // console.log("fleet>>>>>>>>>>>>>>>>>>>>")
                 fleetsData(fleetArray).then(fleetList => {
                     if (fleetArray != null && fleetArray.length != 0) {
                         driverData(driverArray).then(driverList=>{
@@ -150,13 +94,13 @@ async function fleetsData(fleet) {
 
 
 async function fleetfields(fleets) {
-    console.log('fleets', fleets)
+    // console.log('fleets', fleets)
     let sentResponse = {};
     let newFleetData = {};
     return new Promise(function (resolve, reject) {
         fleet.findOne({ _id: fleets.fleetId }, (error, result) => {
             // console.log("error>>>>>>>>>>>" + error)
-            console.log("??????????fleet result" , result)
+            // console.log("??????????fleet result" , result)
             if (error) {
                 sentResponse.error = true;
                 sentResponse.message = `Error :` + error.message + "Fleet Does not exist";
@@ -166,14 +110,16 @@ async function fleetfields(fleets) {
             else if (result) {
                 // console.log('resultcoming ',result)
                 for (var key in fleets){
-                    // console.log(result[key])
+                    // console.log('result[key] fleet',result[key])
+                    // console.log('result[key] fleet',key)
+
                     newFleetData[key] = result[key];
                     delete newFleetData.fleetId;
                     newFleetData.fleetId=result._id;
                     newFleetData.userId=result.userId;
                     newFleetData.ownership=result.ownership;
                 }
-                console.log('resolve(newFleetData[0])', newFleetData)
+                // console.log('resolve(newFleetData[0])', newFleetData)
                 resolve(newFleetData)
 
 
@@ -204,7 +150,7 @@ async function driverfields(driver) {
     return new Promise(function (resolve, reject) {
         drivers.findById({ _id: driver.driverId }, (error, result) => {
             // console.log("error>>>>>>>>>>>" + error)
-            console.log("??????????fleet result" , result)
+            // console.log("??????????fleet result" , result)
             if (error) {
                 sentResponse.error = true;
                 sentResponse.message = `Error :` + error.message + "Driver Does not exist";
@@ -220,7 +166,7 @@ async function driverfields(driver) {
                     newDriverData.driverId=result._id;
                     newDriverData.userId=result.userId;
                 }
-                console.log('resolve(newDriverData[0])', newDriverData)
+                // console.log('resolve(newDriverData[0])', newDriverData)
                 resolve(newDriverData)
 
 
@@ -229,4 +175,69 @@ async function driverfields(driver) {
         })
     })
 }
+/************************************END ******************************************** */
 module.exports = router;
+     // console.log("result>>>>>>>>>>>>" + result)       
+            // if (companyData.gst == 'gst') {
+            //     console.log("gst")
+            //     newCompanyData.push({ gst: result.gst })
+            // }
+            // if (companyData.road_registration_certificate == 'road_registration_certificate') {
+            //     console.log("road_registration_certificate")
+            //     newCompanyData.push({ road_registration_certificate: result.road_registration_certificate })
+            // }
+            // if (companyData.tradeLicense_A == 'tradeLicense_A') {
+            //     console.log("tradeLicense_A")
+            //     newCompanyData.push({ tradeLicense_A: result.tradeLicense_A })
+
+            // }
+            // if (companyData.tradeLicense_B == 'tradeLicense_B') {
+            //     console.log("tradeLicense_B")
+            //     newCompanyData.push({ tradeLicense_B: result.tradeLicense_B })
+
+            // }
+            // if (companyData.invoice_number == 'invoice_number') {
+            //     console.log("invoice_number")
+            //     newCompanyData.push({ invoice_number: result.invoice_number })
+
+            // }
+            // if (companyData.pan == 'pan') {
+            //     console.log("pan")
+            //     newCompanyData.push({ pan: result.pan })
+
+            // }
+            // if (companyData.tan == 'tan') {
+            //     console.log("tan")
+            //     newCompanyData.push({ tan: result.tan })
+
+            // }
+            // if (companyData.pf == 'pf') {
+            //     console.log("pf")
+            //     newCompanyData.push({ pf: result.pf })
+
+            // }
+            // if (companyData.professional_tax == 'professional_tax') {
+            //     console.log("professional_tax")
+            //     newCompanyData.push({ professional_tax: result.professional_tax })
+
+            // }
+            // if (companyData.esi == 'esi') {
+            //     console.log("esi")
+            //     newCompanyData.push({ esi: result.esi })
+
+            // }
+            // if (companyData.itr == 'itr') {
+            //     console.log("itr")
+            //     newCompanyData.push({ itr: result.itr })
+
+            // }
+            // if (companyData.balance_sheet == 'balance_sheet') {
+            //     console.log("balance_sheet")
+            //     newCompanyData.push({ balance_sheet: result.balance_sheet })
+
+            // }
+            // if (companyData.others == 'others') {
+            //     console.log("others")
+            //     newCompanyData.push({ balance_sheet: result.others })
+
+            // }
